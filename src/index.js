2dde92555fe6cbdf7698794b9c36fcd336437351
Gastón Marchetta <gaston.marchetta@gmail.com>
2020-07-08 01:59:46 -0300

    Adding functional frontend using react.

---
+++ src/index.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import axios from 'axios';
+
+class Basket extends React.Component {
+  render() {
+    return (
+      <div className="basket">
+        <h1>Your basket contents:</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td width="150">Product name</td>
+              <td width="150">Product type</td>
+              <td width="150">Price</td>
+              <td width="150">Quantity</td>
+            </tr>
+            {this.props.basket.items.map((item, index) => {
+              return <BasketItem key={index} item={item} />
+            })}
+          </tbody>
+        </table>
+
+        <h3>Total amount after discounts: {this.props.basket.totalAmount}</h3>
+      </div>
+    );
+  }
+}
+
+class BasketItem extends React.Component {
+  render() {
+    return (
+      <tr>
+          <td>{this.props.item.product.productName}</td>
+          <td>{this.props.item.product.productType}</td>
+          <td>{this.props.item.product.price}</td>
+          <td>{this.props.item.quantity}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductAppender extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Add products here!</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                Product
+              </td>
+              <td>
+                Quantity
+              </td>
+              <td>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <select ref={this.props.productRef} id='product' name='product'>
+                  <option value="1">Lana Pen</option>
+                  <option value="2">Lana T-Shirt</option>
+                  <option value="3">Lana Coffee Mug</option>
+                </select>
+              </td>
+              <td>
+                <input ref={this.props.quantityRef}/>
+              </td>
+              <td>
+                <button className="add-product" onClick={() => this.props.onAddProduct(1, 2)}>
+                  Add to cart
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class Checkout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: null,
+      items: [],
+      totalAmount: 0
+    };
+  }
+
+  componentDidMount() {
+    axios.post('http://localhost:8080/basket', {})
+      .then(res => {
+        const basket = res.data;
+        const id = basket.id;
+        const items = basket.items != undefined? basket.items: [];
+        this.setState({id: id, items: items});
+        axios.get('http://localhost:8080/basket/' + id + '/total')
+          .then(res => {
+            const total = res.data;
+            this.setState({ totalAmount: total });
+          });
+      });
+
+  }
+
+  handleAddProduct() {
+    const productId = this.productId.value;
+    const quantity = this.quantity.value;
+
+    axios.post('http://localhost:8080/basket/' + this.state.id + '/products', {productId, quantity})
+      .then(res => {
+        const basket = res.data;
+        this.setState({ items: basket.basketItems });
+        axios.get('http://localhost:8080/basket/' + this.state.id + '/total')
+          .then(res => {
+            const total = res.data;
+            this.setState({ totalAmount: total });
+          });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Basket basket={this.state}/>
+        <ProductAppender
+          basketId={this.state.id}
+          onAddProduct={()=>this.handleAddProduct()}
+          quantityRef={(quantityInput) => this.quantity = quantityInput}
+          productRef={(productSelect) => this.productId = productSelect}
+        />
+      </div>
+    )
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Checkout />,
+  document.getElementById('root')
+);
